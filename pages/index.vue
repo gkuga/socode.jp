36d983bb65f57adfe9af4e43270db518defc97ea
gkuga <kuga@vivaldi.net>
2018-08-19 11:55:39 +0000

    marquee追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
   <h1>爽コード</h1>
-    <hr>
-      ようこそ爽コードへ
+  <hr>
+  <div class="marquee">
+    <p>ようこそ爽コードへ</p>
+  </div>
   </div>
 </template>
+
+<style>
+.marquee {
+  background:aliceblue;
+  overflow:hidden;
+  margin:1em 0;
+}
+
+.marquee p {
+  margin:0;
+  padding-left:100%;
+  display:inline-block;
+  white-space:nowrap;
+  animation-name:marquee;
+  animation-duration:10s;
+    animation-timing-function:linear;
+    animation-iteration-count:infinite;
+}
+
+@keyframes marquee {
+  from { transform:translate(0);     }
+  to   { transform:translate(-100%); }
+}
+</style>
